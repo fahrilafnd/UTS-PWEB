72cbd6ccf2114cf4b79f50ebc04c2b875ca0181d
fahrilafnd <167693774+fahrilafnd@users.noreply.github.com>
2024-10-18 23:47:18 +0700

    Add files via upload

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+<div class="bg-gradient-to-tr from-gray-900 to-gray-500">
+  <section id="login" class="p-4 flex flex-col justify-center min-h-screen max-w-md mx-auto">
+    <div class="p-6 bg-white rounded">
+      <div class="flex items-center justify-center text-4xl font-black text-grey-900 m-3">
+        <svg class="w-12 h-12" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M364.61 390.213C304.625 450.196 207.37 450.196 147.386 390.213C117.394 360.22 102.398 320.911 102.398 281.6C102.398 242.291 117.394 202.981 147.386 172.989C147.386 230.4 153.6 281.6 230.4 307.2C230.4 256 256 102.4 294.4 76.7999C320 128 334.618 142.997 364.608 172.989C394.601 202.981 409.597 242.291 409.597 281.6C409.597 320.911 394.601 360.22 364.61 390.213Z" fill="#4C51BF" stroke="#4C51BF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M201.694 387.105C231.686 417.098 280.312 417.098 310.305 387.105C325.301 372.109 332.8 352.456 332.8 332.8C332.8 313.144 325.301 293.491 310.305 278.495C295.309 263.498 288 256 275.2 230.4C256 243.2 243.201 320 243.201 345.6C201.694 345.6 179.2 332.8 179.2 332.8C179.2 352.456 186.698 372.109 201.694 387.105Z" fill="white"></path>
+        </svg>
+      <h1 class="tracking-wide">META JAYA <span class="font-mono">™</span></h1>
+      </div>
+        <form id="login_form" action="api_login" method="POST"
+        class="flex flex-col justify-center">
+          <label class="text-sm font-medium">First Name</label>
+          <input class="mb-3 px-2 py-1.5
+          mb-3 mt-1 block w-full px-2 py-1.5 border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
+          focus:outline-none
+          focus:border-sky-500
+          focus:ring-1
+          focus:ring-sky-500
+          focus:invalid:border-red-500 focus:invalid:ring-red-500" type="text" name="First Name" placeholder="First Name" required>
+          <label class="text-sm font-medium">Last Name</label>
+          <input class="mb-3 px-2 py-1.5
+          mb-3 mt-1 block w-full px-2 py-1.5 border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
+          focus:outline-none
+          focus:border-sky-500
+          focus:ring-1
+          focus:ring-sky-500
+          focus:invalid:border-red-500 focus:invalid:ring-red-500" type="text" name="Last Name" placeholder="Last Name" required>
+          <label class="text-sm font-medium">Email</label>
+          <input class="mb-3 px-2 py-1.5
+          mb-3 mt-1 block w-full px-2 py-1.5 border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
+          focus:outline-none
+          focus:border-sky-500
+          focus:ring-1
+          focus:ring-sky-500
+          focus:invalid:border-red-500 focus:invalid:ring-red-500" type="text" name="Email" placeholder="Email" required>
+          <label class="text-sm font-medium">Password</label>
+          <input class="
+          mb-3 mt-1 block w-full px-2 py-1.5 border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
+          focus:outline-none
+          focus:border-sky-500
+          focus:ring-1
+          focus:ring-sky-500
+          focus:invalid:border-red-500 focus:invalid:ring-red-500" type="password" name="password" placeholder="********" required>
+          <label class="text-sm font-medium">Confirm Password</label>
+          <input class="
+          mb-3 mt-1 block w-full px-2 py-1.5 border border-gray-300 rounded-md text-sm shadow-sm placeholder-gray-400
+          focus:outline-none
+          focus:border-sky-500
+          focus:ring-1
+          focus:ring-sky-500
+          focus:invalid:border-red-500 focus:invalid:ring-red-500" type="password" name="Confirm Password" placeholder="********" required>
+    
+            <button class="px-4 py-1.5 rounded-md shadow-lg bg-sky-600 font-medium text-gray-100 block hover:bg-sky-700 transition duration-300" onclick="window.location.href = 'http://127.0.0.1:8000/dashboard/admin'">Register</button>
+            
+        </form>
+    </div>
+  </section>
+
+  <script>
+    document.getElementById("login_form").onsubmit = function() {
+      event.preventDefault()
+      // animation
+      document.getElementById("login_process_state").classList.remove("hidden")
+      document.getElementById("login_process_state").classList.add("animate-pulse")
+
+      document.getElementById("login_default_state").classList.add("hidden")
+    }
+
+  </script>
+</div>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\AdminDashboardController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard/admin', [AdminDashboardController::class, 'index']);
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
